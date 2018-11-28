f1f0a5bf5f70b4911a12229134eeb00420e1a7a0
Oskar Nylin <oskar.nylin@learnet.se>
2018-11-28 09:15:45 +0100

    more stuff

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <script src="js/jquery-3.3.1.min.js"></script>
+</head>
+<body>
+        
+</body>
+</html>
