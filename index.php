60e37f9d1903c855e10249408de8e8626df71849
Oskar Nylin <oskar.nylin@learnet.se>
2018-11-28 15:22:02 +0100

    create post

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+    include "utility/utility.php";
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,10 +10,51 @@
     <script src="js/jquery-3.3.1.min.js"></script>
 </head>
 <body>
+    <form action="" method="post">
+        <input name="username" placeholder="Username">
+        <input name="password" placeholder="Password">
+        <input type="submit" value="Submit">
+    </form>
+    <?php if(isset($_POST["username"]) && isset($_POST["password"])){
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+            $response = myCurl::execute("http://10.130.216.144/~theprovider/generate-token.php",
+            [
+                "username"=>$username,
+                "password"=>$password,
+            ]);
+            
+            $token = json_decode($response,true);
+            $_SESSION["token"] = $token["token"];
+            $account = json_decode($response,true);
+            $_SESSION["account"] = $account["accountID"];
+    } ?>
+
     <div class="flow">
-        <?php include "get-all-posts.php";?>
-        <div class="commentfield">(foreach)
-        </div>
+        <form action="" method="post">
+            <input name="blogID" placeholder="BlogID">
+            <input name="title" placeholder="Title">
+            <input name="date" type="date">
+            <input name="content" placeholder="Text here...">
+            <input type="submit" value="Submit">   
+        </form>
+        <?php if(isset($_POST["blogID"]) && isset($_POST["title"]) && isset($_POST["date"]) && isset($_POST["content"])){
+            $blog = $_POST["blogID"];
+            $title = $_POST["title"];
+            $date = $_POST["date"];
+            $content = $_POST["content"];
+            
+            $response = myCurl::execute("http://10.130.216.144/~theprovider/blog/php/create-post.php",
+            [
+                "token"=>$_SESSION["token"],
+                "accountID"=>$_SESSION["account"],
+                "blogID"=>$blog,
+                "title"=>$title,
+                "date"=>$date,
+                "content"=>$content
+            ]);
+            var_dump($response);
+        } ?>
     </div>
 </body>
 </html>
