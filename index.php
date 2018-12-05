b5dfe9dcce2fd2a064e2de9f8ccc08bc5fff3b38
CasperEhlin <casper.ehlin@hotmail.se>
2018-12-05 11:41:37 +0100

    added local jquery & added sass

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/index-sass.css">
     <script src="js/jquery-3.3.1.min.js"></script>
 </head>
 <body>
